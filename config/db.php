ea6e2b7a0e7e9d3a3835be387c845dd02741296f
unknown <jiayi1172@gmail.com>
2019-11-14 21:02:29 +0800

    Updated database configuration

--- config/db.php
+++ config/db.php
@@ -7,9 +7,8 @@
     $username = 'kjoiwgdfgwrxbf';
     $password = '3678cd37386aa59354c4478a4ea6d4b4016d16b49d83a6585fa07ec907d19dc5';
     $database = 'd23m1kun1tmsvj';
-    $port = 5432;
 
-    $conn = mysqli_connect($server, $username, $password, $database, $port);
+    $conn = mysqli_connect($server, $username, $password, $database);
 
     if(!$conn){
         die("Connection failed " . mysqli_connect_error());
