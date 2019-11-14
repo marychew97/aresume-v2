b4019bf40b875a95a536c7078399c4418f3d19fc
unknown <jiayi1172@gmail.com>
2019-11-14 20:58:49 +0800

    Updated database configuration

--- config/db.php
+++ config/db.php
@@ -4,12 +4,12 @@
     // $password = 'lamp-aws';
     // $database = 'aresume';
     $server = 'ec2-174-129-253-53.compute-1.amazonaws.com';
-    $host = 'kjoiwgdfgwrxbf';
+    $username = 'kjoiwgdfgwrxbf';
     $password = '3678cd37386aa59354c4478a4ea6d4b4016d16b49d83a6585fa07ec907d19dc5';
     $database = 'd23m1kun1tmsvj';
     $port = 5432;
 
-    $conn = mysqli_connect($server, $host, $password, $database, $port);
+    $conn = mysqli_connect($server, $username, $password, $database, $port);
 
     if(!$conn){
         die("Connection failed " . mysqli_connect_error());
