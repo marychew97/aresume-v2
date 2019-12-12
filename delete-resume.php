7b473b92de520fbc0015c09a6a258277edac9f94
unknown <jiayi1172@gmail.com>
2019-12-12 16:41:43 +0800

    Updated code with database table

--- delete-resume.php
+++ delete-resume.php
@@ -5,7 +5,7 @@
     $resume_id = $_POST['resume_id'];
     $id = $_SESSION['id'];
     
-    $sql = "DELETE FROM resume WHERE resume_id = $resume_id AND user_id = $id";
+    $sql = "DELETE FROM template_temp WHERE resume_id = $resume_id AND user_id = $id";
     $result = mysqli_query($conn, $sql);
 
     if($result){
